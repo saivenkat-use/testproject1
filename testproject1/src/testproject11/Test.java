76e2eaab72b321bf2e7032be7bfa5af9d3734c6a
saivenkat-use <katarisaivenkat18@gamil.com>
2024-07-31 14:45:16 +0530

    updatein test.java

--- testproject1/src/testproject11/Test.java
+++ testproject1/src/testproject11/Test.java
@@ -2,7 +2,7 @@ package testproject11;
 
 public class Test {
 	public static void main(String[] args) {
-		 System.out.println("updated successfully");
+		 System.out.println("updated successfully yeswanth");
 	}
 
 }
